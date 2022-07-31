35bbe95b7c9eb7b3448b0546fb96377641939e47
Arman Sarvadin <78083072+armansarv07@users.noreply.github.com>
2022-08-01 00:59:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIDefinitive
+
+A description of this package.
---
+++ Sources/SwiftUIDefinitive/SwiftUIDefinitive.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIDefinitive {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
